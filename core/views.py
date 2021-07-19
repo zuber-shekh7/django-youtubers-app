04e8e3a490403ed14f254803345aa190b99745bf
Zuber Shekh <zuber.shekh7@gmail.com>
2021-07-19 22:33:48 +0530

    feature: add about page

--- core/views.py
+++ core/views.py
@@ -9,3 +9,7 @@ def index(request):
         'sliders': sliders,
     }
     return render(request, 'core/index.html', context)
+
+
+def about(request):
+    return render(request, 'core/about.html')
